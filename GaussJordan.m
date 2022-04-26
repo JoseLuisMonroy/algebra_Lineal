12fc194dcd9da36fb39ab87e065a7a0702db79c4
Jose <mnry7999@gmail.com>
2022-04-26 13:29:46 -0500

    add: GaussJordan.m
    Se agrega el archivo de Gauss jordan Completado

---
+++ GaussJordan.m
@@ -0,0 +1,87 @@
+function [MatrixFinal] = GaussJordan(matrixExtend)
+%Gauss Jordan 
+%   Mteodo de gauss Jordan para la matrices 
+    multiplicator = matrixExtend(2, 1) / matrixExtend(1, 1);
+    % Uptdate Matrix B
+    if multiplicator > 0
+
+        for i = 1:4
+            matrixExtend(2, i) = matrixExtend(2, i) + (matrixExtend(1, i) * -multiplicator);
+        end
+
+    else
+
+        for i = 1:4
+            matrixExtend(2, i) = matrixExtend(2, i) + (matrixExtend(1, i) * multiplicator);
+        end
+
+    end
+
+    %disp(matrixExtend); %In order to show step by step remove the first' %'
+    multiplicator = matrixExtend(3, 1) / matrixExtend(1, 1);
+    %Uptdate matrix B
+    if multiplicator > 0
+
+        for i = 1:4
+            matrixExtend(3, i) = matrixExtend(3, i) + (matrixExtend(1, i) * -multiplicator);
+        end
+
+    else
+
+        for i = 1:4
+            matrixExtend(3, i) = matrixExtend(3, i) + (matrixExtend(1, i) * multiplicator);
+        end
+
+    end
+
+    %disp(matrixExtend); %In order to show step by step remove the first' %'
+    multiplicator = matrixExtend(3, 2) / matrixExtend(2, 2);
+    %Uptdate matrix B
+    if multiplicator > 0
+
+        for i = 2:4
+            matrixExtend(3, i) = matrixExtend(3, i) + (matrixExtend(2, i) * -multiplicator);
+        end
+
+    else
+
+        for i = 2:4
+            matrixExtend(3, i) = matrixExtend(3, i) + (matrixExtend(2, i) * multiplicator);
+        end
+
+    end
+
+    %disp(matrixExtend); %In order to show step by step remove the first' %'
+    multiplicator = matrixExtend(1, 2) / matrixExtend(2, 2);
+    %Uptdate matrix B
+
+        for i = 2:4
+            matrixExtend(1, i) = matrixExtend(1, i) + (matrixExtend(2, i) * -multiplicator);
+        end
+    
+    %disp(matrixExtend); %In order to show step by step remove the first' %'
+    multiplicator = matrixExtend(2, 3) / matrixExtend(3, 3);
+    %Uptdate matrix B
+
+    for i = 3:4
+        matrixExtend(2, i) = matrixExtend(2, i) + (matrixExtend(3, i) * -multiplicator);
+    end
+
+    %disp(matrixExtend); %In order to show step by step remove the first' %'
+    multiplicator = matrixExtend(1, 3) / matrixExtend(3, 3);
+    %Uptdate matrix B
+
+    for i = 3:4
+        matrixExtend(1, i) = matrixExtend(1, i) + (matrixExtend(3, i) * -multiplicator);
+    end
+    
+    for i = 1:3
+        multiplicator = 1 / matrixExtend (i, i);
+        matrixExtend(i, i) = matrixExtend(i,i) * multiplicator;
+        matrixExtend(i, 4) = matrixExtend(i,4) * multiplicator;
+    end
+
+    disp(matrixExtend);
+
+    MatrixFinal = matrixExtend;
+end
